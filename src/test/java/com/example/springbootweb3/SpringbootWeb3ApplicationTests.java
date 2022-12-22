77c0ba1ac74c13a86fd503c7911917121263eb99
Junhan93 <junhan.3008@gmail.com>
2022-12-22 13:12:38 +0800

    initial commit

---
+++ src/test/java/com/example/springbootweb3/SpringbootWeb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootweb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWeb3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
